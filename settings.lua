402242ea3d9442f317eed31c9cd936f18c074537
nozwock <57829219+nozwock@users.noreply.github.com>
2024-10-19 12:29:56 +0530

    Remove redundant category from settings

--- settings.lua
+++ settings.lua
@@ -3,67 +3,58 @@ dofile("data/scripts/lib/mod_settings.lua")
 -- Use ModSettingGet() in the game to query settings.
 local mod_id = "health_container"
 mod_settings_version = 1
-mod_settings = 
-{
+mod_settings = {
   {
-    category_id = "health_container_settings",
-    ui_name = "Health Container Settings",
-    ui_description = "Settings for the health container drops.",
-    settings = 
-    {
-      {
-        id = "drop_chance",
-        ui_name = "Drop Chance",
-        ui_description = "Chance that a health container drops upon killing an enemy.",
-        value_default = 0.1,
-        value_min = 0,
-        value_max = 1,
-        value_display_multiplier = 100,
-        value_display_formatting = " $0 %",
-        scope = MOD_SETTING_SCOPE_RUNTIME,
-      },	  
-      {
-        id = "max_health_increase",
-        ui_name = "Max Health Increase",
-        ui_description = "Amount to increase the max player health by. Takes effect on new game.",
-        value_default = 0,
-        value_min = 0,
-        value_max = 4,
-        value_display_multiplier = 25,
-        value_display_formatting = " $0 HP",
-        scope = MOD_SETTING_SCOPE_NEW_GAME,
-      },
-      {
-        id = "heal_mode",
-        ui_name = "Health Container Healing Mode",
-        ui_description = "Set whether health containers heal a fixed amount or a percent of max health.",
-        value_default = "fixed",
-        values = { {"percent", "Percent"}, {"fixed", "Fixed"} },
-        scope = MOD_SETTING_SCOPE_RUNTIME,
-      },
-      {
-        id = "heal_amount",
-        ui_name = "Healing Amount (Fixed)",
-        ui_description = "Fixed amount of HP to heal each pickup.",
-        value_default = 0.4,
-        value_min = 0,
-        value_max = 4,
-        value_display_multiplier = 25,
-        value_display_formatting = " $0 HP",
-        scope = MOD_SETTING_SCOPE_RUNTIME,
-      },
-      {
-        id = "heal_percent",
-        ui_name = "Healing Amount (Percentage)",
-        ui_description = "Percentage of Max HP to heal on each pickup.",
-        value_default = 0.10,
-        value_min = 0,
-        value_max = 1,
-        value_display_multiplier = 100,
-        value_display_formatting = " $0 %",
-        scope = MOD_SETTING_SCOPE_RUNTIME,
-      }
-    }
+    id = "drop_chance",
+    ui_name = "Drop Chance",
+    ui_description = "Chance that a health container drops upon killing an enemy.",
+    value_default = 0.1,
+    value_min = 0,
+    value_max = 1,
+    value_display_multiplier = 100,
+    value_display_formatting = " $0 %",
+    scope = MOD_SETTING_SCOPE_RUNTIME,
+  },	  
+  {
+    id = "max_health_increase",
+    ui_name = "Max Health Increase",
+    ui_description = "Amount to increase the max player health by. Takes effect on new game.",
+    value_default = 0,
+    value_min = 0,
+    value_max = 4,
+    value_display_multiplier = 25,
+    value_display_formatting = " $0 HP",
+    scope = MOD_SETTING_SCOPE_NEW_GAME,
+  },
+  {
+    id = "heal_mode",
+    ui_name = "Health Container Healing Mode",
+    ui_description = "Set whether health containers heal a fixed amount or a percent of max health.",
+    value_default = "fixed",
+    values = { {"percent", "Percent"}, {"fixed", "Fixed"} },
+    scope = MOD_SETTING_SCOPE_RUNTIME,
+  },
+  {
+    id = "heal_amount",
+    ui_name = "Healing Amount (Fixed)",
+    ui_description = "Fixed amount of HP to heal each pickup.",
+    value_default = 0.4,
+    value_min = 0,
+    value_max = 4,
+    value_display_multiplier = 25,
+    value_display_formatting = " $0 HP",
+    scope = MOD_SETTING_SCOPE_RUNTIME,
+  },
+  {
+    id = "heal_percent",
+    ui_name = "Healing Amount (Percentage)",
+    ui_description = "Percentage of Max HP to heal on each pickup.",
+    value_default = 0.10,
+    value_min = 0,
+    value_max = 1,
+    value_display_multiplier = 100,
+    value_display_formatting = " $0 %",
+    scope = MOD_SETTING_SCOPE_RUNTIME,
   }
 }
 
