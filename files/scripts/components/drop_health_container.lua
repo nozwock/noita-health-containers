a741d52cad37892b1fda51ece3cc215155ca1b60
nozwock <57829219+nozwock@users.noreply.github.com>
2024-10-21 15:59:38 +0530

    Implement logic for new setting options

--- files/scripts/components/drop_health_container.lua
+++ files/scripts/components/drop_health_container.lua
@@ -1,15 +1,29 @@
 dofile_once("data/scripts/game_helpers.lua")
 dofile_once("data/scripts/lib/utilities.lua")
+local const = dofile_once("mods/health_container/files/scripts/const.lua") ---@type const
+local utils = dofile_once("mods/health_container/files/scripts/utils.lua") ---@type utils
 
-function do_health_drop()
-  local entity = GetUpdatedEntityID()
-  local x, y = EntityGetTransform(entity)
+function death(damage_type_bit_field, damage_message, entity_thats_responsible, drop_items)
+  local entity_id = GetUpdatedEntityID()
 
-  if GameGetIsTrailerModeEnabled() then return end
+  local damage_model = EntityGetFirstComponent(entity_id, "DamageModelComponent")
+  if not damage_model then return end
 
-  if math.random() < ModSettingGet("health_container.drop_chance_constant") then
-    EntityLoad("mods/health_container/files/entities/items/health_container.xml", x, y - 7)
+  local max_hp = ComponentGetValue2(damage_model, "max_hp")
+
+  local drop_chance = 0
+  local drop_chance_mode = utils:ModSettingGetNumber("drop_chance_mode")
+  if drop_chance_mode == const.enum.DROP_CHANCE_MODE.CONSTANT then
+    drop_chance = utils:ModSettingGetNumber("drop_chance_constant")
+  elseif drop_chance_mode == const.enum.DROP_CHANCE_MODE.ENEMY_SCALE then
+    local base_chance = utils:ModSettingGetNumber("drop_chance_scale_base")
+    local base_hp = utils:ModSettingGetNumber("drop_chance_scale_base_hp")
+    drop_chance = max_hp / base_hp * base_chance
   end
-end
 
-function death(damage_type_bit_field, damage_message, entity_thats_responsible, drop_items) do_health_drop() end
+  if GameGetIsTrailerModeEnabled() or math.random() > drop_chance then return end
+
+  local x, y = EntityGetTransform(entity_id)
+  local pickup_entity_id = EntityLoad("mods/health_container/files/entities/items/health_container.xml", x, y - 7)
+  EntityAddComponent2(pickup_entity_id, "VariableStorageComponent", { name = "enemy_max_hp", value_float = max_hp })
+end
--- files/scripts/components/health_container_pickup.lua
+++ files/scripts/components/health_container_pickup.lua
@@ -1,68 +1,66 @@
 dofile_once("data/scripts/game_helpers.lua")
 dofile_once("data/scripts/lib/utilities.lua")
 local const = dofile_once("mods/health_container/files/scripts/const.lua") ---@type const
+local utils = dofile_once("mods/health_container/files/scripts/utils.lua") ---@type utils
 
 function item_pickup(entity_item, entity_who_picked, item_name)
-  local function TruncateHpValue(value)
-    -- this function reduces inaccuracy building up over time due to how HP is handled and
-    -- how this version of lua seems to handle floating point values.
-    -- This function truncates values less than 1 HP (<0.04)
-    local shifted = value * 100
-    local truncated = shifted - (shifted % 4)
-    return truncated / 100
+  local damage_model = EntityGetFirstComponent(entity_who_picked, "DamageModelComponent")
+  if not damage_model then return end
+
+  local player_hp = ComponentGetValue2(damage_model, "hp")
+  local player_max_hp = ComponentGetValue2(damage_model, "max_hp")
+
+  local hp_gain_player_scale = utils:ModSettingGetNumber("hp_gain_player_scale")
+
+  ---Scale `hp_gain` with player's maximum HP.
+  ---@param hp_gain number
+  ---@return number
+  local function scale_hp_gain(hp_gain)
+    return hp_gain * (player_max_hp - const.BASE_PLAYER_HP) / const.BASE_PLAYER_HP * hp_gain_player_scale
   end
 
-  local pos_x, pos_y = EntityGetTransform(entity_item)
+  local hp_gain_mode = utils:ModSettingGetNumber("hp_gain_mode")
 
-  local damagemodels = EntityGetComponent(entity_who_picked, "DamageModelComponent")
+  local vsc = utils:EntityGetFirstVSC(entity_item, "enemy_max_hp")
+  if not vsc then return end
+  local enemy_max_hp = ComponentGetValue2(vsc, "value_float")
 
-  if damagemodels ~= nil then
-    for i, v in ipairs(damagemodels) do
-      local current_hp = ComponentGetValue2(v, "hp")
-
-      local current_max_hp = ComponentGetValue2(v, "max_hp")
-      local heal_amt = 0
-      local mode = ModSettingGet("health_container.hp_gain_mode")
-
-      if mode == const.enum.HP_GAIN_MODE.PLAYER_HP_FRACTION then
-        heal_amt = current_max_hp * (ModSettingGet("health_container.hp_gain_fraction_player"))
-      else
-        heal_amt = ModSettingGet("health_container.hp_gain_constant") -- Health values are scaled up by 25 in the UI apparently.
-      end
-
-      heal_amt = math.max(heal_amt, 0.04) -- set heal_amt to be at least 1 HP
-      local target_hp = TruncateHpValue(current_hp + heal_amt)
-
-      -- handle expansion of max HP:
-      local max_incr_amt = ModSettingGet("health_container.max_hp_gain")
-
-      if max_incr_amt > 0 then
-        max_incr_amt = math.max(0.04, max_incr_amt) -- set increase amt to at least 1 HP
-      end
-
-      local target_max_hp = TruncateHpValue(current_max_hp + max_incr_amt)
-
-      -- Save values
-      ComponentSetValue2(v, "max_hp", target_max_hp)
-
-      if target_hp > target_max_hp then target_hp = target_max_hp end
-      ComponentSetValue2(v, "hp", target_hp)
-      GamePrint(string.format("Picked up Health (%d)", TruncateHpValue(heal_amt) * 25))
-      break
-    end
+  local hp_gain = 0
+  if hp_gain_mode == const.enum.HP_GAIN_MODE.CONSTANT then
+    hp_gain = utils:ModSettingGetNumber("hp_gain_constant")
+    hp_gain = hp_gain + scale_hp_gain(hp_gain)
+  elseif hp_gain_mode == const.enum.HP_GAIN_MODE.PLAYER_HP_FRACTION then
+    hp_gain = player_max_hp * utils:ModSettingGetNumber("hp_gain_fraction_player")
+  elseif hp_gain_mode == const.enum.HP_GAIN_MODE.ENEMY_HP_FRACTION then
+    hp_gain = utils:ModSettingGet("hp_gain_fraction_enemy_constant")
+      + enemy_max_hp * utils:ModSettingGetNumber("hp_gain_fraction_enemy")
+    hp_gain = hp_gain + scale_hp_gain(hp_gain)
   end
 
-  GamePlaySound(
-    "mods/health_container/files/audio/health_container_audio.snd",
-    "health_container/hc_heal",
-    pos_x,
-    pos_y
-  )
+  local max_hp_gain = utils:ModSettingGetNumber("max_hp_gain")
+
+  local hp_gain_variation = utils:ModSettingGetNumber("hp_gain_variation_percentage")
+  if hp_gain_variation < 1 then -- 100% means no variation
+    hp_gain_variation = utils:RandomFloat(hp_gain_variation, 1)
+    hp_gain = hp_gain * hp_gain_variation
+    max_hp_gain = max_hp_gain * hp_gain_variation
+  end
+
+  if max_hp_gain > 0 then
+    player_max_hp = player_max_hp + max_hp_gain
+    ComponentSetValue2(damage_model, "max_hp", player_max_hp)
+  end
+
+  ComponentSetValue2(damage_model, "hp", player_hp + hp_gain)
+  GamePrint(string.format("Picked up Health (%.1f)", hp_gain * 25))
+
+  local x, y = EntityGetTransform(entity_item)
+  GamePlaySound("mods/health_container/files/audio/health_container_audio.snd", "health_container/hc_heal", x, y)
   shoot_projectile(
     entity_item,
     "mods/health_container/files/entities/particles/health_container_pickup.xml",
-    pos_x,
-    pos_y,
+    x,
+    y,
     0,
     0
   )
