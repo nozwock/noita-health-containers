91de223a3442c0812fce47c23a49f4e2213e09f1
nozwock <57829219+nozwock@users.noreply.github.com>
2024-10-22 14:55:22 +0530

    Add custom material for health pickup item
    Derived from gem_box2d, the same one gold nuggets utilise.

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 ModRegisterAudioEventMappings("mods/health_container/files/audio/audio_events.txt")
+ModMaterialsFileAdd("mods/health_container/files/materials.xml")
 
-function OnModInit()
-  dofile_once("mods/health_container/files/scripts/on_init/appends.lua")
-end
+function OnModInit() dofile_once("mods/health_container/files/scripts/on_init/appends.lua") end
